8bd0961c4f82e80e865a1c84a75381d5c9e25ccb
Kenaidi01 <andrianp920@gmail.com>
2025-01-12 20:09:08 +0700

    anykernel: Add support for A15

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=IMMEENSITY-X KERNEL
+kernel.string=NIGEL KERNEL
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -15,7 +15,7 @@ device.name2=munchin
 device.name3=
 device.name4=
 device.name5=
-supported.versions=12.0-14.0
+supported.versions=12.0-15.0
 supported.patchlevels=
 '; } # end properties
 
